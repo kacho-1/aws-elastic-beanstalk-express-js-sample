7b5dc5a1df9fa6f6b0d2cd9e6569d0b3f4593147
Kacho Kamran <94922512+kacho-1@users.noreply.github.com>
2024-11-07 12:17:17 +0500

    Add files via upload
    change message

---
+++ app.js.js
@@ -0,0 +1,11 @@
+// Logging a message to the console
+console.log("Hello, world!");
+
+// Function to add two numbers
+function addNumbers(a, b) {
+    return a + b;
+}
+
+// Using the function
+let result = addNumbers(3, 5);
+console.log("The result is: " + result);
